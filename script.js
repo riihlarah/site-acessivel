e184650b5453d41fbb5d5ecbba499ba19ee64089
Rita Lara <144171818+riihlarah@users.noreply.github.com>
2025-03-28 19:34:19 +0000

    contraste pt3

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
+    const alternaContraste = this.getElementById('alterna-contraste')
+
     botaoDeAcessibilidade.addEventListener('click', function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
@@ -18,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
-
-
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+    })
 });
